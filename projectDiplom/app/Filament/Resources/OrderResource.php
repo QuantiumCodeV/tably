929cf730379693275670ff79b81b7694bc0912f8
QuantiumCodeV <quantiumcode@mail.ru>
2025-03-06 20:27:26 -0600

    fix

--- projectDiplom/app/Filament/Resources/OrderResource.php
+++ projectDiplom/app/Filament/Resources/OrderResource.php
@@ -115,6 +115,7 @@ class OrderResource extends Resource
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
